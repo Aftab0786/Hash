742ea29bc6cff943a92f53f46c9862200b149494
Aftab-1998 <alam41976@gmail.com>
2022-02-16 12:59:34 +0530

    [AFTAB1] Add; Added the method to find frequency

--- Hash/Program.cs
+++ Hash/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome To Hash Table!");
+﻿using System;
+using System.Collections.Generic;
+namespace HashTable
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hash Table");
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(19);
+            hash.Add("0", "To");
+            hash.Add("1", "be");
+            hash.Add("2", "or");
+            hash.Add("3", "not");
+            hash.Add("4", "to");
+            hash.Add("5", "be");
+
+            string hash5 = hash.Get("5");
+            Console.WriteLine("5th index value: " + hash5);
+        }
+    }
+}
